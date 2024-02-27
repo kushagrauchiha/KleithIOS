6b33b592f40dd9ab1cdbb24f7ef3fd993393c338
kushagraa14 <kushagra@clevertap.com>
2024-02-27 16:40:08 +0530

    Created iOS Project with Hello World as label

---
+++ KleithiOS/KleithiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KleithiOS
+//
+//  Created by Kushagra Mishra on 27/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
